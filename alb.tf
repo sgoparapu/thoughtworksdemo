fd63f824c3ecffefa1279baa15704c0f5f7560ed
sgoparapu <34513908+sgoparapu@users.noreply.github.com>
2019-07-18 00:27:32 +0530

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -1,5 +1,3 @@
-#### Creating Security Group for Application Load Balancer
-
 resource "aws_security_group" "tw-alb" {
   name = "tw-alb"
   vpc_id = "${aws_vpc.tw-vpc.id}"
@@ -31,8 +29,6 @@ resource "aws_alb" "tw-alb01" {
   security_groups = ["${aws_security_group.tw-alb.id}"]
 }
 
-### Creating Target Group for Application Load Balancer
-
 resource "aws_alb_target_group" "tw-group" {
   name     = "tw-alb-target"
   port     = 80
@@ -47,8 +43,6 @@ resource "aws_alb_target_group" "tw-group" {
   }
 }
 
-### Creating Listener for Application Load Balancer - Telling on which port to listen
-
 resource "aws_alb_listener" "listener_http" {
   load_balancer_arn = "${aws_alb.tw-alb01.arn}"
   port              = "80"
@@ -60,12 +54,11 @@ resource "aws_alb_listener" "listener_http" {
   }
 }
 
-### Attaching Instances to the ALB as the requirement is 2 instances only we are not using Auto Scaling rather attaching directly
-
 resource "aws_alb_target_group_attachment" "attach0" {
   target_group_arn = "${aws_alb_target_group.tw-group.arn}"
   target_id = "${aws_instance.web01.id}"
 }
+
 resource "aws_alb_target_group_attachment" "attach1" {
   target_group_arn = "${aws_alb_target_group.tw-group.arn}"
   target_id = "${aws_instance.web02.id}"
