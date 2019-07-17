3651fc7dafb9b7cbcf37c9f5e37e3f0151aa4175
sgoparapu <34513908+sgoparapu@users.noreply.github.com>
2019-07-18 00:24:29 +0530

    Update web.sh

--- userdata/web.sh
+++ userdata/web.sh
@@ -1,9 +1,3 @@
-#### This script is used to install git,ansible and then set the hostname
-#### As this is a web node we are applying the httpd playbook
-#### This will also clone the code and playbooks from git and execute during the instance startup
-#### Here we are using a master less ansible as this satisfies the requirement
-#### For adhoc deployments and pull code we can set a scheduled cron to pull code or setup a ansible master and create a playbook accordingly
-
 #!/bin/bash
 sudo yum install git -y
 cd /home/ec2-user/
